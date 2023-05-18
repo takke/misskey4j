a7ab63501c262316692bb6c3e6ca11131971598a
takke <takke30@gmail.com>
2023-05-18 21:22:28 +0900

    Add some properties to Notification

--- src/main/java/misskey4j/entity/Notification.java
+++ src/main/java/misskey4j/entity/Notification.java
@@ -1,52 +1,72 @@
 package misskey4j.entity;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Notification {
 
-    private String id;
-    private String createdAt;
-    private String type;
+    @NotNull
+    private String id = "";
 
-    private String userId;
+    @NotNull
+    private String createdAt = "";
+
+    @NotNull
+    private String type = "";
 
     @Nullable
     private User user;
+
+    @Nullable
+    private String userId;
+
+    @Nullable
     private Note note;
 
+    @Nullable
     private String reaction;
 
+    @Nullable
+    private Long choice;
+
+    // TODO parse invitation object
+
+    @Nullable
+    private String body;
+
+    @Nullable
+    private String header;
+
+    @Nullable
+    private String icon;
+
+
     // region
+    @NotNull
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(@NotNull String id) {
         this.id = id;
     }
 
+    @NotNull
     public String getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(String createdAt) {
+    public void setCreatedAt(@NotNull String createdAt) {
         this.createdAt = createdAt;
     }
 
+    @NotNull
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(@NotNull String type) {
         this.type = type;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
     }
 
     @Nullable
@@ -58,20 +78,67 @@ public class Notification {
         this.user = user;
     }
 
+    @Nullable
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(@Nullable String userId) {
+        this.userId = userId;
+    }
+
+    @Nullable
     public Note getNote() {
         return note;
     }
 
-    public void setNote(Note note) {
+    public void setNote(@Nullable Note note) {
         this.note = note;
     }
 
+    @Nullable
     public String getReaction() {
         return reaction;
     }
 
-    public void setReaction(String reaction) {
+    public void setReaction(@Nullable String reaction) {
         this.reaction = reaction;
+    }
+
+    @Nullable
+    public Long getChoice() {
+        return choice;
+    }
+
+    public void setChoice(@Nullable Long choice) {
+        this.choice = choice;
+    }
+
+    @Nullable
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(@Nullable String body) {
+        this.body = body;
+    }
+
+    @Nullable
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(@Nullable String header) {
+        this.header = header;
+    }
+
+    @Nullable
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(@Nullable String icon) {
+        this.icon = icon;
     }
     // endregion
 }
